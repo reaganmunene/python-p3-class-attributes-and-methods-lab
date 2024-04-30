306a2d0362819b9c5b9b5ebe3eed0b5a4437e343
reaganmunene <reagannmunene@gmail.com>
2024-04-30 21:52:20 +0300

    reference notes

--- lib/song.py
+++ lib/song.py
@@ -16,12 +16,12 @@ class Song:
         self.add_to_artist_count()
 
     @classmethod
-    def add_song_to_count(cls):
+    def add_song_to_count(cls): #note: the class method takes cls as the arguement.
         cls.count += 1
 
     def add_to_genres(self):
         if self.genre not in Song.genres:
-            Song.genres.append(self.genre)
+            Song.genres.append(self.genre) #note that it is Song.genres(accessing the class itself using dot notation, so we mention the class.) and not self.genres that is being appended.
 
     def add_to_artists(self):
         if self.artist not in Song.artists:
@@ -42,3 +42,9 @@ class Song:
                 cls.artist_count[song] += 1
             else:
                 cls.artist_count[song] = 1
+
+## class RESOURCES:
+# - https://docs.python.org/3/
+# - https://www.toptal.com/python/python-class-attributes-an-overly-thorough-guide
+# - https://realpython.com/instance-class-and-static-methods-demystified/
+# - https://realpython.com/factory-method-python/
